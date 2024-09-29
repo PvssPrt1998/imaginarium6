ae31397a4a5ec8638c72a73c1135a2baea959db4
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-09-29 16:15:04 +0300

    Initial Commit

---
+++ imaginarium/imaginariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  imaginariumApp.swift
+//  imaginarium
+//
+//  Created by Николай Щербаков on 29.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct imaginariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
